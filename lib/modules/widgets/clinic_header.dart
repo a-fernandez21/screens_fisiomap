b0b17c7dd75a6ff6d1c0b9e8401930d626c0b474
a-fernandez21 <afernandez1@pumpunhost.com>
2025-10-28 17:24:43 +0100

    feat(intl):add extension flutter intl

---
+++ lib/modules/widgets/clinic_header.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../../constants/app_colors.dart';
+import '../../constants/app_text_styles.dart';
+import '../../constants/app_constants.dart';
+
+/// Widget que muestra el encabezado de la clínica FisioMap.
+/// 
+/// Incluye un gradiente de fondo, un ícono de hospital, el nombre de la clínica
+/// y un subtítulo con la descripción del servicio. Este widget es reutilizable
+/// en diferentes pantallas de la aplicación.
+class ClinicHeader extends StatelessWidget {
+  const ClinicHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [AppColors.primaryLight, AppColors.primary],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(AppConstants.borderRadius),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(AppConstants.shadowOpacity),
+            spreadRadius: AppConstants.shadowSpreadRadius,
+            blurRadius: AppConstants.shadowBlurRadius,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: const Column(
+        children: [
+          Icon(
+            Icons.local_hospital,
+            color: AppColors.textLight,
+            size: AppConstants.iconSize,
+          ),
+          SizedBox(height: AppConstants.smallSpacing),
+          Text(
+            AppConstants.clinicName,
+            style: AppTextStyles.clinicName,
+          ),
+          Text(
+            AppConstants.clinicSubtitle,
+            style: AppTextStyles.clinicSubtitle,
+          ),
+        ],
+      ),
+    );
+  }
+}
